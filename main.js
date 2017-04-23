b85e0a2ccb62f5c937e60f220678390cc75d3ae6
mobicom777 <j.mobicom@gmail.com>
2017-04-23 11:32:37 +1000

    Update main.js

--- main.js
+++ main.js
@@ -9,24 +9,11 @@
                   };
  
  
-                var myPlayer = amp("azuremediaplayer", myOptions, function () {
-                        //resize-to-fit handler code goes here if you’re doing full screen background video
-                  });
- 
-                  //Register for events after intialization not in Ready function to ensure all event are captured
-                  myPlayer.addEventListener(amp.eventName.play, _ampEventHandler);
-                  myPlayer.addEventListener(amp.eventName.ended, _ampEventHandler);
-           
+                var myPlayer = amp("azuremediaplayer", myOptions)
                   myPlayer.src([
                         { src: "//amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest" },
                   ]);
  
-                  function _ampEventHandler(evt) {
-                        if ("play" == evt.type) {
-                              $('#video-viewport').css("opacity", "1");
-                        }
-                        if ("ended" == evt.type) {
-                              myPlayer.play();
-                        }
-                  }
+                 
+          
      
