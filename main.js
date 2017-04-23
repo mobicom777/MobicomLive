bf846d87ef4b2bbe96ff2e3e59986ecdea120368
mobicom777 <j.mobicom@gmail.com>
2017-04-23 12:56:08 +1000

    55

--- main.js
+++ main.js
@@ -1,19 +1,22 @@
 
-                  var myOptions = {
-                        heuristicProfile: "HighQuality",
-                        techOrder: ["azureHtml5JS", "html5", "flashSS", "silverlightSS", ],
-                        "nativeControlsForTouch": false,
-                        autoplay: true,
-                        controls: false,
-                        logo: { enabled: false }
-                  };
- 
- 
-                var myPlayer = amp("azuremediaplayer", myOptions)
-                  myPlayer.src([
-                        { src: "//amssamples.streaming.mediaservices.windows.net/91492735-c523-432b-ba01-faba6c2206a2/AzureMediaServicesPromo.ism/manifest" },
-                  ]);
- 
-                 
-          
+     var myPlayer = amp('azuremediaplayer', { /* Options */
+        techOrder: ["azureHtml5JS", "flashSS", "html5FairPlayHLS","silverlightSS", "html5"],
+        "nativeControlsForTouch": false,
+        autoplay: false,
+        controls: true,
+        width: "640",
+        height: "400",
+        poster: ""
+    }, function() {
+          console.log('Good to go!');
+           // add an event listener
+          this.addEventListener('ended', function() {
+            console.log('Finished!');
+        }
+      }
+);
+myPlayer.src([{ 
+    src: "http://mobicom.streaming.mediaservices.windows.net/4082234e-de8e-46f6-bff0-01df846795e8/b35e7f08-a06c-46dc-9a9a-504b4f1360d3.ism/manifest", 
+    type: "application/vnd.ms-sstr+xml"
+}]);
      
